f3d29fce97c6408955b834a6a16dbf028ff2cc7d
mhmttpkay <mhmt.tpkay12@gmail.com>
2021-09-25 17:13:10 +0300

    Todo-app-React
    Todo app (React)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const INITIAL_STATE = [
+  { id: 1, baslik: "Alisveris Yap", tamamlandi: false },
+  { id: 2, baslik: "Fatura ode", tamamlandi: true }
+];
+
+export default function App() {
+  const [liste, setListe] = useState(INITIAL_STATE);
+  const [yeniBaslik, setYeniBaslik] = useState("");
+  
+  const newtitle = title => {
+    if (title ==='') {
+      alert('Bu kısım boş bırakılamaz.')
+    } else {
+      setListe([...liste, { id: Date.now(), baslik: title, tamamlandi: false }]);
+      setYeniBaslik('');
+    }
+
+  }
+
+  const clickEvent= (id)=>{
+    setListe(
+      liste.map(el => el.id === id ? { ...el, tamamlandi: !el.tamamlandi } : el
+      )
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Yapılacaklar Listesi</h1>
+      <div className="ekleme_formu">
+
+        <input
+          value={yeniBaslik}
+          onChange={e => setYeniBaslik(e.target.value)}
+          placeholer="listeye ekle" />
+
+        <button
+          onClick={() => {
+            newtitle(yeniBaslik);
+
+          }}>Ekle</button>
+      </div>
+      <div className="liste">
+        {liste.map(item => (
+          <div key={item.id} onClick={() => {
+            clickEvent(item.id);
+          }}
+            className={item.tamamlandi ? "yapildi" : ""}>{item.baslik}</div>
+        ))}
+      </div>
+      <button
+        className="temizle"
+        onClick={() => {
+          setListe(liste.filter(item => !item.tamamlandi))
+        }}
+      >Tamamlananları Temizle</button>
     </div>
   );
 }
-
-export default App;
